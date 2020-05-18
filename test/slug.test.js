88fc85dc35379b3534bf7776dd52c1edb0f158e6
Rich Trott <rtrott@gmail.com>
2020-05-18 05:25:29 -0700

    feat: support Devanagari for Hindi, Sanskrit, and other languages (#55)

--- test/slug.test.js
+++ test/slug.test.js
@@ -520,6 +520,79 @@ describe('slug', function () {
     }
   })
 
+  it('should replace hindi chars', function () {
+    const charMap = {
+      अ: 'a',
+      आ: 'aa',
+      ए: 'e',
+      ई: 'ii',
+      ऍ: 'ei',
+      ऎ: 'ae',
+      ऐ: 'ai',
+      इ: 'i',
+      ओ: 'o',
+      ऑ: 'oi',
+      ऒ: 'oii',
+      ऊ: 'uu',
+      औ: 'ou',
+      उ: 'u',
+      ब: 'B',
+      भ: 'Bha',
+      च: 'Ca',
+      छ: 'Chha',
+      ड: 'Da',
+      ढ: 'Dha',
+      फ: 'Fa',
+      फ़: 'Fi',
+      ग: 'Ga',
+      घ: 'Gha',
+      ग़: 'Ghi',
+      ह: 'Ha',
+      ज: 'Ja',
+      झ: 'Jha',
+      क: 'Ka',
+      ख: 'Kha',
+      ख़: 'Khi',
+      ल: 'L',
+      ळ: 'Li',
+      ऌ: 'Li',
+      ऴ: 'Lii',
+      ॡ: 'Lii',
+      म: 'Ma',
+      न: 'Na',
+      ङ: 'Na',
+      ञ: 'Nia',
+      ण: 'Nae',
+      ऩ: 'Ni',
+      ॐ: 'oms',
+      प: 'Pa',
+      क़: 'Qi',
+      र: 'Ra',
+      ऋ: 'Ri',
+      ॠ: 'Ri',
+      ऱ: 'Ri',
+      स: 'Sa',
+      श: 'Sha',
+      ष: 'Shha',
+      ट: 'Ta',
+      त: 'Ta',
+      ठ: 'Tha',
+      द: 'Tha',
+      थ: 'Tha',
+      ध: 'Thha',
+      ड़: 'ugDha',
+      ढ़: 'ugDhha',
+      व: 'Va',
+      य: 'Ya',
+      य़: 'Yi',
+      ज़: 'Za'
+    }
+    for (const char in charMap) {
+      const replacement = charMap[char]
+      assert.strictEqual(slug(`foo ${char} bar baz`), `foo-${replacement}-bar-baz`.toLowerCase())
+    }
+  })
+
   it('should replace serbian chars', function () {
     const charMap = { ђ: 'dj', ј: 'j', љ: 'lj', њ: 'nj', ћ: 'c', џ: 'dz', đ: 'dj', Ђ: 'Dj', Ј: 'j', Љ: 'Lj', Њ: 'Nj', Ћ: 'C', Џ: 'Dz', Đ: 'Dj', ǉ: 'lj', ǋ: 'NJ', ǈ: 'LJ' }
     for (const char in charMap) {

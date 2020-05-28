65d21f87eeb5c9f821a334f4128188b5624b297d
Rich Trott <rtrott@gmail.com>
2020-05-27 19:28:14 -0700

    feat: add support for Georgian characters

--- test/slug.test.js
+++ test/slug.test.js
@@ -593,6 +593,48 @@ describe('slug', function () {
     }
   })
 
+  it('should replace georgian chars', function () {
+    const charMap = {
+      ა: 'a',
+      ბ: 'b',
+      გ: 'g',
+      დ: 'd',
+      ე: 'e',
+      ვ: 'v',
+      ზ: 'z',
+      თ: 't',
+      ი: 'i',
+      კ: 'k',
+      ლ: 'l',
+      მ: 'm',
+      ნ: 'n',
+      ო: 'o',
+      პ: 'p',
+      ჟ: 'zh',
+      რ: 'r',
+      ს: 's',
+      ტ: 't',
+      უ: 'u',
+      ფ: 'p',
+      ქ: 'k',
+      ღ: 'gh',
+      ყ: 'q',
+      შ: 'sh',
+      ჩ: 'ch',
+      ც: 'ts',
+      ძ: 'dz',
+      წ: 'ts',
+      ჭ: 'ch',
+      ხ: 'kh',
+      ჯ: 'j',
+      ჰ: 'h'
+    }
+    for (const char in charMap) {
+      const replacement = charMap[char]
+      assert.strictEqual(slug(`foo ${char} bar baz`), `foo-${replacement}-bar-baz`.toLowerCase(), `replacing '${char}'`)
+    }
+  })
+
   it('should replace bulgarian chars if locale provided', function () {
     const charMap = {
       A: 'A',

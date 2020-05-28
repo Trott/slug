a7cf67505b6e37b8e925076d694cfa9ca64caae0
Rich Trott <rtrott@gmail.com>
2020-05-27 21:04:09 -0700

    feat: add support for Slovak characters

--- test/slug.test.js
+++ test/slug.test.js
@@ -329,6 +329,49 @@ describe('slug', function () {
     }
   })
 
+  it('should replace slovak chars', function () {
+    const charMap = {
+      á: 'a',
+      ä: 'a',
+      č: 'c',
+      ď: 'd',
+      é: 'e',
+      í: 'i',
+      ľ: 'l',
+      ĺ: 'l',
+      ň: 'n',
+      ó: 'o',
+      ô: 'o',
+      ŕ: 'r',
+      š: 's',
+      ť: 't',
+      ú: 'u',
+      ý: 'y',
+      ž: 'z',
+      Á: 'a',
+      Ä: 'A',
+      Č: 'C',
+      Ď: 'D',
+      É: 'E',
+      Í: 'I',
+      Ľ: 'L',
+      Ĺ: 'L',
+      Ň: 'N',
+      Ó: 'O',
+      Ô: 'O',
+      Ŕ: 'R',
+      Š: 'S',
+      Ť: 'T',
+      Ú: 'U',
+      Ý: 'Y',
+      Ž: 'Z'
+    }
+    for (const char in charMap) {
+      const replacement = charMap[char]
+      assert.strictEqual(slug(`foo ${char} bar baz`), `foo-${replacement}-bar-baz`.toLowerCase(), `replacing '${char}'`)
+    }
+  })
+
   it('should replace polish chars', function () {
     const charMap = {
       ą: 'a',

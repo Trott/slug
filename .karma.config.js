b67cd309995c01ee2dc1d00032d019fb60cca63c
Rich Trott <rtrott@gmail.com>
2020-05-29 06:32:44 -0700

    chore: add coverage reports for browser tests

--- .karma.config.js
+++ .karma.config.js
@@ -1,22 +1,29 @@
 module.exports = function (config) {
   config.set({
-      frameworks: ['mocha', 'chai'],
-      files: [
-          'slug.js',
-          'test/**/*.js'
-      ],
-      reporters: ['progress'],
-      port: 9876,
-      colors: true,
-      logLevel: config.LOG_INFO,
-      browsers: ['ChromeHeadless', 'Firefox'],
-      autoWatch: false,
-      concurrency: Infinity,
-      customLaunchers: {
-          FirefoxHeadless: {
-              base: 'Firefox',
-              flags: ['-headless'],
-          },
-      },
+    frameworks: ['mocha', 'chai'],
+    files: [
+      'slug.js',
+      'test/**/*.js'
+    ],
+    preprocessors: { 'slug.js': 'coverage' },
+    reporters: ['coverage'],
+    coverageReporter: {
+      dir: '.nyc_output',
+      reporters: [
+        { type: 'json', subdir: '.', file: 'karma.json' }
+      ]
+    },
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    browsers: ['ChromeHeadless', 'Firefox'],
+    autoWatch: false,
+    concurrency: Infinity,
+    customLaunchers: {
+      FirefoxHeadless: {
+        base: 'Firefox',
+        flags: ['-headless']
+      }
+    }
   })
 }

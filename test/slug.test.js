26499bfb0f50be30e273a9772c75e1203d24c223
Rich Trott <rtrott@gmail.com>
2020-08-12 15:42:54 -0700

    feat: add support for Hebrew

--- test/slug.test.js
+++ test/slug.test.js
@@ -1074,4 +1074,63 @@ describe('slug', function () {
     slug.reset()
     checkAll(charmap, multicharmap, assert.deepStrictEqual)
   })
+
+  it('should replace hebrew', function () {
+    const charMap = {
+      א: '',
+      בּ: 'b',
+      ב: 'v',
+      גּ: 'g',
+      ג: 'g',
+      ד: 'd',
+      דּ: 'd',
+      ה: 'h',
+      ו: 'v',
+      ז: 'z',
+      ח: 'h',
+      ט: 't',
+      י: 'y',
+      כּ: 'k',
+      ךּ: 'k',
+      כ: 'kh',
+      ך: 'kh',
+      ל: 'l',
+      מ: 'm',
+      ם: 'm',
+      נ: 'n',
+      ן: 'n',
+      ס: 's',
+      ע: '',
+      פּ: 'p',
+      פ: 'f',
+      ף: 'f',
+      ץ: 'ts',
+      צ: 'ts',
+      ק: 'k',
+      ר: 'r',
+      שׁ: 'sh',
+      שׂ: 's',
+      תּ: 't',
+      ת: 't',
+      בְ: 'e',
+      חֱ: 'e',
+      חֲ: 'a',
+      חֳ: 'o',
+      בִ: 'i',
+      בִי: 'i',
+      בֵ: 'e',
+      בֵי: 'e',
+      בֶ: 'e',
+      בַ: 'a',
+      בָ: 'a',
+      בֹ: 'o',
+      וֹ: 'o',
+      בֻ: 'u',
+      וּ: 'u'
+    }
+    for (let char in charMap) { // eslint-disable-line prefer-const
+      const replacement = charMap[char]
+      assert.strictEqual(slug('foo' + char + ' bar baz'), 'foo' + replacement.toLowerCase() + '-bar-baz', 'replacing \'' + char + '\'')
+    }
+  })
 })

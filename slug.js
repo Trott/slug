81a8161b6827bf0baf9e85a45b9c3721d416c335
Rich Trott <rtrott@gmail.com>
2021-04-20 22:08:33 -0700

    chore: simplify regexp

--- slug.js
+++ slug.js
@@ -206,8 +206,7 @@
       result = result.replace(opts.remove, '')
     }
     result = result.trim()
-    result = result.replace(/[-\s]+/g, opts.replacement) // convert spaces
-    // result = result.replace(opts.replacement + '$', '') // remove trailing separator
+    result = result.replace(/[\s]+/g, opts.replacement) // convert spaces
     if (opts.lower) {
       result = result.toLowerCase()
     }

a4dca294bc1398ded86456403ccc30bee67d6e02
Rich Trott <rtrott@gmail.com>
2022-04-20 13:09:18 -0700

    chore: ESM only
    BREAKING CHANGE: module is now ESM only

--- test/browser-mock.test.js
+++ test/browser-mock.test.js
@@ -7,17 +7,21 @@ if (typeof window === 'undefined') {
     assert = (await import('node:assert')).default
   })()
 
-  describe('React Native-like environment', function () {
+  describe('Browser-like environment', function () {
     before(function () {
-      global.window = {}
+      global.window = global
+      // Stub it for Node.js 14.
+      if (!global.btoa) {
+        const returnValues = ['6bOE5qKo', 'IoMBvw==', 'IA==', 'IA=+']
+        global.btoa = () => returnValues.shift()
+      }
     })
     after(function () {
       delete global.window
     })
 
     it('should work for window object with no btoa function', async function () {
-      assert.strictEqual(window.btoa, undefined)
-      const slug = (await import('../slug.js?cachebustingreactnative')).default
+      const slug = (await import('../slug.js?cachebustingbrowser')).default
       assert.strictEqual(slug('鳄梨'), '6boe5qko')
       assert.strictEqual(slug(String.fromCodePoint(56714, 36991)), 'iombvw')
       assert.strictEqual(slug(String.fromCodePoint(56714)), 'ia')

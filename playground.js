dfe2f339c6b926751b2f224af2001f9a1eb09330
Siah <siah@goodwork.sg>
2024-05-05 10:20:28 +0900

    feat: playground with some options

---
+++ playground.js
@@ -0,0 +1,40 @@
+const form = document.getElementById("form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (form === null) {
+    console.error("form element not found");
+    return;
+  }
+
+  const fd = new FormData(form);
+
+  const [replacement, remove, regex_g, regex_i, lowercase, trim] = Array.from(
+    fd.values()
+  );
+
+  const opts = {};
+
+  if (replacement.length === 1) {
+    opts.replacement = replacement;
+  }
+
+  try {
+    if (regex_g !== undefined) {
+    }
+    const regex = new RegExp(
+      remove,
+      `${regex_g !== undefined ? "g" : ""}${regex_i !== undefined ? "i" : ""}`
+    );
+    opts.remove = regex;
+  } catch (err) {
+    console.error(err);
+  }
+
+  opts.lower = lowercase !== undefined;
+
+  opts.trim = trim !== undefined;
+
+  const output = slug(document.getElementById("input").value, opts);
+  document.getElementById("slugOutput").innerHTML = output;
+});

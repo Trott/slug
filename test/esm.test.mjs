8e0629cab280f84792907828bccbacb6644f494a
Rich Trott <rtrott@gmail.com>
2024-10-08 22:04:05 -0700

    chore: update esm.test.mjs to use chai

--- test/esm.test.mjs
+++ test/esm.test.mjs
@@ -1,10 +1,10 @@
 /* global describe, it */
 
 import slug from '../slug.js'
-import assert from 'assert'
+import { expect } from '@esm-bundle/chai'
 
 describe('esm wrapper', function () {
   it('should slugify strings', function () {
-    assert.strictEqual(slug('foo bar baz'), 'foo-bar-baz')
+    expect(slug('foo bar baz')).to.equal('foo-bar-baz')
   })
 })
